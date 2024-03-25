4635d167b3400a5e83d2a162db97696169a8e3bf
Anutej Poddaturi <poddaturi.a@northeastern.edu>
2024-03-25 15:32:46 -0400

    Adds Context and Makes Danjob show upon signin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,18 +14,19 @@ const Navbar = async (props: Props) => {
       <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
         <Link href="/gallery" className="items-center hidden gap-2 sm:flex">
           <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
-            AssistedInstruct.ai
+            Husky.ai
           </p>
         </Link>
         <div className="flex items-center">
           <Link href="/gallery" className="mr-3">
             Gallery
           </Link>
-          <Link href="/danjob" className="mr-3">
-            Danjob
-          </Link>
+          
           {session?.user && (
             <>
+              <Link href="/danjob" className="mr-3">
+              Danjob
+              </Link>
               <Link href="/create" className="mr-3">
                 Create Course
               </Link>
