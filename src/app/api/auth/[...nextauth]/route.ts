24283b530bb1ac3dac266dcac478de4aa5f22753
Anutej Poddaturi <113458527+apoddaturi@users.noreply.github.com>
2024-04-23 19:32:25 -0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,3 +4,5 @@ import NextAuth from "next-auth/next";
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+//This file sets up NextAuth, a popular authentication library for Next.js applications.
